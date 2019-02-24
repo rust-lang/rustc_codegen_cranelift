6da42e485031ee521fe85eec875c388fea456f35
bjorn3 <bjorn3@users.noreply.github.com>
2019-02-24 18:57:09 +0100

    [WIP]

--- test.sh
+++ test.sh
@@ -80,10 +80,10 @@ cat > config.toml <<EOF
 enabled = false
 [build]
 local-rebuild = true
-rustc = "/home/bjorn/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin/rustc"
+rustc = "$HOME/.rustup/toolchains/nightly-x86_64-apple-darwin/bin/rustc"
 EOF
 
 #rm -r build/x86_64-unknown-linux-gnu/test || true
 ./x.py test --stage 0 src/test/run-pass/ \
-    --rustc-args "-Zcodegen-backend=/home/bjorn/Documenten/rustc_codegen_cranelift/target/debug/librustc_codegen_cranelift.so --sysroot /home/bjorn/Documenten/rustc_codegen_cranelift/build_sysroot/sysroot -Cpanic=abort" \
-    |& tee log.txt
+    --rustc-args "-Zcodegen-backend=$(pwd)/../target/"$channel"/librustc_codegen_cranelift."$dylib_ext" --sysroot $(pwd)/../build_sysroot/sysroot -Cpanic=abort" \
+    2>&1 | tee log.txt
