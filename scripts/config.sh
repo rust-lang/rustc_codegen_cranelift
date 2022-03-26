2cd171c77a4dfba3b54e87cfe824214e7b1d8c7c
bjorn3 <bjorn3@users.noreply.github.com>
2022-03-26 10:50:35 +0100

    [WIP]

--- scripts/config.sh
+++ scripts/config.sh
@@ -4,5 +4,4 @@ set -e
 
 export LD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH"
 export DYLD_LIBRARY_PATH="$(rustc --print sysroot)/lib:$DYLD_LIBRARY_PATH"
-export PATH="$(rustc --print sysroot)/bin;$PATH"
 ls $(rustc --print sysroot)/bin
