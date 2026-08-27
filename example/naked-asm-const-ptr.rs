5aaf685e22fa6c34b5afb4402313a6dcf0783bd4
Rudraksh Joshi <rudrakshjoshic@gmail.com>
2026-08-27 20:38:41 +0530

    use s390x retrn instruction in naked-asm-const-ptr test

--- example/naked-asm-const-ptr.rs
+++ example/naked-asm-const-ptr.rs
@@ -7,7 +7,11 @@ use std::arch::naked_asm;
 
 #[unsafe(naked)]
 extern "C" fn naked() {
+    // `ret` is x86-specific; s390x returns via `br %r14` (see `has_mnemonic` in src/lib.rs).
+    #[cfg(not(target_arch = "s390x"))]
     naked_asm!("ret /* {} */", const &0);
+    #[cfg(target_arch = "s390x")]
+    naked_asm!("br %r14 /* {} */", const &0);
 }
 
 fn main() {
