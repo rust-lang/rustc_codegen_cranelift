c4a61825f13ce16746111ff837dc16a544d6699b
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2026-09-18 12:14:48 +0200

    Revert change

--- scripts/rustup.sh
+++ scripts/rustup.sh
@@ -17,6 +17,7 @@ case $1 in
         for nightly in $(rustup toolchain list | grep nightly | grep -v "$TOOLCHAIN" | grep -v nightly-x86_64); do
             rustup toolchain uninstall "$nightly"
         done
+
         ./clean_all.sh
 
         ./y.sh prepare
